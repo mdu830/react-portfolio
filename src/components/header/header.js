768d4f40665182fdefe36155bf6081d1f5d03141
mdu830 <mdu830@yahoo.com>
2021-05-02 17:26:06 -0400

    added styling to navbar links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,17 +5,17 @@ function Header() {
         <>
             <div className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 
-                <div className="navbar-link nav-text" id="aboutMeBtn" ><a href="/about">Mitchell Duncan</a></div>
+                <div className="nav-link navText" id="aboutMeBtn" ><a href="/about">Mitchell Duncan</a></div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <nav className="collapse navbar-collapse text-right" id="navbarNavDropdown">
                     <ul className="navbar-nav ml-auto pl-0">
                     <li className="nav-item">
-                        <div className="nav-link nav-text" id="portfolioBtn"><a href="/portfolio">Portfolio</a></div>
+                        <div className="nav-link navText" id="portfolioBtn"><a href="/portfolio">Portfolio</a></div>
                     </li>
                     <li className="nav-item">
-                        <div className="nav-link nav-text" id="contactBtn"><a href="/contact">Contact</a></div>
+                        <div className="nav-link navText" id="contactBtn"><a href="/contact">Contact</a></div>
                     </li>
                     </ul>
                 </nav>
