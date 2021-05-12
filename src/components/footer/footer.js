fc13f955c6081cf08f342f65765a7fec7e1398fa
mdu830 <mdu830@yahoo.com>
2021-05-12 15:49:45 -0400

    removed border radius from footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 
 function Footer() {
     return (
-        <footer className="footer bord fixed-bottom">
+        <footer className="footer borderr fixed-bottom">
             <div className="footerContainer text-center pt-1">
                 <span className="footer-text"> &#169; Mitchell Duncan 2021</span>
             </div>
