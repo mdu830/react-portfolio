36fb9884cb6f786df06afa1971cf27dc57853a13
mdu830 <mdu830@yahoo.com>
2021-06-28 19:16:30 -0400

    added reactstrap

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -6,30 +6,33 @@ import { zoomIn } from 'react-animations'
 
 const Animate = styled.div`animation: 1s ${keyframes`${zoomIn}`} `;
 
+
+
 function Portfolio() {
+
   console.log(portfolioItems);
   return (
     <div>
       <Header />
-      <Animate>
-        <article className="media m-5 mt-5 mb-4 bord">
-          <div className="media-body m-1">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-12">
-                  <h5 className="title ml-3 mt-5 mb-4">Portfolio</h5>
-                </div>
+
+      <article className="media m-5 mt-5 mb-4 bord">
+        <div className="media-body m-1">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <h5 className="title ml-3 mt-5 mb-4">Portfolio</h5>
               </div>
-              {portfolioItems && portfolioItems.map((i, propKey) => {
-                return (
-                  <Project key={propKey} feat={i.feat} title={i.title} image={i.image} pTag1={i.pTag1} pTag2={i.pTag2} link={i.link} linkText={i.linkText} pTag3={i.pTag3} github={i.github} />
-                )
-              }
-              )}
             </div>
+            {portfolioItems && portfolioItems.map((i, propKey) => {
+              return (
+                <Animate><Project key={propKey} feat={i.feat} title={i.title} image={i.image} pTag1={i.pTag1} pTag2={i.pTag2} link={i.link} linkText={i.linkText} pTag3={i.pTag3} github={i.github} /></Animate>
+              )
+            }
+            )}
           </div>
-        </article>
-      </Animate>
+        </div>
+      </article>
+
     </div>
   )
 };
