541320d2ed6b63da0148d49a66aef86d604f6c7b
mdu830 <mdu830@yahoo.com>
2021-06-29 14:29:41 -0400

    converted header to reactstrap from bootstrap

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,43 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem
+} from 'reactstrap';
 
-function Header() {
+
+const Header = (props) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <>
-            <div className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <Navbar className="fixed-top py-0" color="dark" dark expand="lg">
+                <NavbarBrand><Link className="nav-link navText" to="/about">Mitchell Duncan</Link></NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+
+                <Collapse isOpen={isOpen}  navbar>
+                    <Nav className="ml-auto justify-content-right" right>
+                        <NavItem className="nav-link navText" id="portfolioBtn">
+                            <a target="_blank" href="./assets/Resume.pdf">Resume</a>
+                        </NavItem>
+                        <NavItem className="nav-link navText" id="portfolioBtn">
+                            <Link to="/portfolio">Portfolio</Link>
+                        </NavItem>
+                        <NavItem className="nav-link navText" id="portfolioBtn">
+                            <Link to="/contact">Contact</Link>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+
+            </Navbar>
+            {/* <div className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 
                 <div className="nav-link navText" id="aboutMeBtn" ><Link to="/about">Mitchell Duncan</Link></div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,11 +55,9 @@ function Header() {
                         <div className="nav-link navText" id="contactBtn"><Link to="/contact">Contact</Link></div>
                     </li>
                     </ul>
-                </nav>
-            </div>
-       </>
+                </nav> */}
+        </>
     );
-  }
-  
-  export default Header;
-  
+}
+
+export default Header;
