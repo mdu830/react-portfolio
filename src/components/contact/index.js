1eb8128f1aec39fd548da45ca6c3f49d50304457
mdu830 <mdu830@yahoo.com>
2021-06-29 16:47:10 -0400

    reformatted contact page

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,37 +1,38 @@
 import Header from '../header/header';
 import styled, { keyframes } from 'styled-components'
-import { rotateInDownRight } from 'react-animations'
+import { fadeInDown, fadeIn } from 'react-animations'
 
-const Animate = styled.div`animation: 1s ${keyframes`${rotateInDownRight}`} `;
+const Animate = styled.div`animation: 1s ${keyframes`${fadeInDown}`} `;
+const Animate2 = styled.div`animation: 3.5s ${keyframes`${fadeIn}`} `;
+
 
 function Contact() {
   return (
     <div>
       <Header />
+      <Animate2>
+        <article className="row pageTitle justify-content-center">
+          <h5 className="title mt-5 mb-4">Contact</h5>
+        </article>
+      </Animate2>
       <Animate>
         <article className="media m-5 mt-5 mb-4 bord box">
           <div className="media-body m-1">
-            <div className="container mr-n3">
-              <div className="row">
-                <div className="col-9">
-                  <h5 className="title ml-2 mt-5 mb-4">Contact</h5>
-                </div>
-                <div className="col-3">
-                  <a href="https://github.com/mdu830"><img className="ico mt-3" alt="Github" src={`${process.env.PUBLIC_URL}/assets/icons/github.png`}></img></a>
-                  <a href="https://www.linkedin.com/in/mitchell-duncan-a466501bb"><img className="ico mt-3" alt="LinkedIn" src={`${process.env.PUBLIC_URL}/assets/icons/linkedin.png`}></img></a>
-                  <a href="mailto:mdu830@yahoo.com"><img className="ico mt-3" alt="Email" src={`${process.env.PUBLIC_URL}/assets/icons/email.png`}></img></a>
-                </div>
+            <div className="container">
+              <div className="row justify-content-center mt-5">
+                <h3 className="text-center">If you have any questions or would like to get in contact with me here is my contact information: </h3>
+
               </div>
-              <div className="row">
-                <div className="col ml-5">
-                  <p className="ml-5">If you have any questions or would like to get in contact with me here is my contact information: </p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-6">
+              <div className="row justify-content-center">
+                <div className="col-4-md">
                   <h6 className="m-5">Email: <a className="bsLink" href="mailto:mdu830@yahoo.com">mdu830@yahoo.com</a></h6>
                 </div>
-                <div className="col-6">
+                <div className="col-4-md">
+                  <a href="https://github.com/mdu830"><img className="ico mt-1 ml-3 mr-3" alt="Github" src={`${process.env.PUBLIC_URL}/assets/icons/github.png`}></img></a>
+                  <a href="https://www.linkedin.com/in/mitchell-duncan-a466501bb"><img className="ico mt-1 ml-3 mr-3" alt="LinkedIn" src={`${process.env.PUBLIC_URL}/assets/icons/linkedin.png`}></img></a>
+                  <a href="mailto:mdu830@yahoo.com"><img className="ico mt-1 ml-3 mr-3" alt="Email" src={`${process.env.PUBLIC_URL}/assets/icons/email.png`}></img></a>
+                </div>
+                <div className="col-4-md">
                   <h6 className="m-5">Phone: <a href="Phone Number" className="bsLink">(252) 207-7476</a></h6>
                 </div>
               </div>
