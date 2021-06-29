80396dbe2fdaa2dd0acae5f074df7051bb0012d9
mdu830 <mdu830@yahoo.com>
2021-06-29 14:54:19 -0400

    adjusted footer height

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@
 function Footer() {
     return (
         <footer className="footer borderr fixed-bottom">
-            <div className="footerContainer text-center pt-1">
+            <div className="footerContainer text-center">
                 <span className="footer-text"> &#169; Mitchell Duncan 2021</span>
             </div>
         </footer>
